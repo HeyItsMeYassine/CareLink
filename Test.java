d6e6c0efe175e257e28e1e81442e072dfc8734c4
Sofia-Noure <sofianoure@gmail.com>
2025-11-13 22:23:19 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-package m;
+package mo;
 
 public class Medecin {
 
@@ -8,14 +8,15 @@ public class Medecin {
     private String specialite;
     private String email;
     private String telephone;
-    private String ville;  
+    private String wilaya;
+    private String ville;
+    private String password; 
 
-    public Medecin() {
-    	//javafx stuff
-    }
+    public Medecin() {}
 
     public Medecin(String id, String nom, String prenom, String specialite,
-                   String email, String telephone, String wilaya, String ville) {
+                   String email, String telephone, String wilaya, String ville,
+                   String password) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
@@ -24,37 +25,34 @@ public class Medecin {
         this.telephone = telephone;
         this.wilaya = wilaya;
         this.ville = ville;
+        this.password = password;
     }
 
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
-
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
-
     public String getPrenom() { return prenom; }
     public void setPrenom(String prenom) { this.prenom = prenom; }
-
     public String getSpecialite() { return specialite; }
     public void setSpecialite(String specialite) { this.specialite = specialite; }
-
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-
     public String getTelephone() { return telephone; }
     public void setTelephone(String telephone) { this.telephone = telephone; }
-
     public String getWilaya() { return wilaya; }
     public void setWilaya(String wilaya) { this.wilaya = wilaya; }
-
     public String getVille() { return ville; }
     public void setVille(String ville) { this.ville = ville; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
     @Override
     public String toString() {
         return "Médecin : " + nom + " " + prenom +
                " | Spécialité : " + specialite +
                " | Wilaya : " + wilaya +
-               " | Ville : " + ville;
+               " | Ville : " + ville +
+               " | Email : " + email;
     }
 }
