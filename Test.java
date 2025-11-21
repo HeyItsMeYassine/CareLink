42a0682cb0f161e14861c333f9e1f04335d4c533
Sofia-Noure <sofianoure@gmail.com>
2025-11-21 16:54:38 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,58 +1,49 @@
-package mo;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalTime;
 
-public class Medecin {
 
+public class Doctor {
     private String id;
     private String nom;
     private String prenom;
     private String specialite;
-    private String email;
-    private String telephone;
-    private String wilaya;
-    private String ville;
-    private String password; 
+    private String ville; 
+    private List<LocalTime> disponibilites; 
 
-    public Medecin() {}
-
-    public Medecin(String id, String nom, String prenom, String specialite,
-                   String email, String telephone, String wilaya, String ville,
-                   String password) {
+    
+    public Doctor(String id, String nom, String prenom, String specialite, String ville) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
         this.specialite = specialite;
-        this.email = email;
-        this.telephone = telephone;
-        this.wilaya = wilaya;
         this.ville = ville;
-        this.password = password;
+        this.disponibilites = new ArrayList<>();
+        initDefaultSlots();
     }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    private void initDefaultSlots() {
+        disponibilites.add(LocalTime.of(9, 0));
+        disponibilites.add(LocalTime.of(10, 0));
+        disponibilites.add(LocalTime.of(14, 0));
+    }
+
     public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
-    public String getPrenom() { return prenom; }
-    public void setPrenom(String prenom) { this.prenom = prenom; }
     public String getSpecialite() { return specialite; }
-    public void setSpecialite(String specialite) { this.specialite = specialite; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getTelephone() { return telephone; }
-    public void setTelephone(String telephone) { this.telephone = telephone; }
-    public String getWilaya() { return wilaya; }
-    public void setWilaya(String wilaya) { this.wilaya = wilaya; }
-    public String getVille() { return ville; }
-    public void setVille(String ville) { this.ville = ville; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public List<LocalTime> getDisponibilites() { return disponibilites; }
+
+    public boolean reserverCreneau(LocalTime heure) {
+        if (disponibilites.contains(heure)) {
+            disponibilites.remove(heure);
+            System.out.println("Créneau de " + heure + " réservé pour Dr. " + nom);
+            // Ici, on pourrait notifier les Observateurs plus tard
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
-        return "Médecin : " + nom + " " + prenom +
-               " | Spécialité : " + specialite +
-               " | Wilaya : " + wilaya +
-               " | Ville : " + ville +
-               " | Email : " + email;
+        return "Dr. " + nom + " " + prenom + " [" + specialite + "] - " + ville;
     }
 }
