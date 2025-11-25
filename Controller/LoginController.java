306474364c78f5e01bcc396cb464aaa77794061e
Sofia-Noure <sofianoure@gmail.com>
2025-11-25 19:40:29 +0100

    Update LoginController.java

--- Controller/LoginController.java
+++ Controller/LoginController.java
@@ -4,11 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField; 
 
 public class LoginController {
 
-    
     @FXML
     private Label emailLabel; 
     
@@ -16,34 +14,24 @@ public class LoginController {
     private TextField emailField; 
     
     @FXML
-    private Button connectButton; 
-    
-    @FXML
     private Button patientButton;
     
     @FXML
-    private Button praticienButton;
-
+    private Button medecinButton;
 
     @FXML
     private void handlePatientClick(ActionEvent event) {
         emailLabel.setText("Email");
         emailField.setPromptText("nom@exemple.com"); 
-        connectButton.setText("Se connecter"); 
-
         patientButton.getStyleClass().add("selected-toggle");
-        praticienButton.getStyleClass().remove("selected-toggle"); 
-     // Remarque : "selected-toggle" est une classe CSS que vous devez définir séparément.
+        medecinButton.getStyleClass().remove("selected-toggle"); 
     }
 
     @FXML
-    private void handlePraticienClick(ActionEvent event) {
+    private void handleMedecinClick(ActionEvent event) {
         emailLabel.setText("Email professionnel");
         emailField.setPromptText("dr.nom@hopital.com");
-        connectButton.setText("Accéder à mon agenda");
-
-        praticienButton.getStyleClass().add("selected-toggle");
+        medecinButton.getStyleClass().add("selected-toggle");
         patientButton.getStyleClass().remove("selected-toggle");
     }
- 
 }
