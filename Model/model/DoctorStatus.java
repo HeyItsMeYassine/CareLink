919e1b497f7cdbda60366f3287849202bcda6795
HeyItsMeYassine <benayacheahmedyassine.extra@gmail.com>
2025-12-19 07:55:02 +0100

    Delete Model/model directory

--- Model/model/DoctorStatus.java
+++
@@ -1,7 +0,0 @@
-package com.carelink.model;
-
-public enum DoctorStatus {
-    PENDING,    // En attente de validation
-    APPROVED,   // Validé
-    REJECTED    // Rejeté
-}
--- Model/model/Observer.java
+++
@@ -1,5 +0,0 @@
-package com.carelink.model;
-
-public interface Observer {
-    void update(String message);
-}
