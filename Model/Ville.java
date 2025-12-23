efc07046df1893ec2d7f58c53d2505bb5b835b2c
HeyItsMeYassine <benayacheahmedyassine.extra@gmail.com>
2025-12-23 02:19:06 +0100

    Delete Model directory

--- Model/Ville.java
+++
@@ -1,9 +0,0 @@
-package com.medisync.utils;
-
-public record Ville(String nom, String wilaya) implements GeographicComponent {
-    @Override
-    public void displayHierarchy(int depth) { 
-        String indent = new String(new char[depth]).replace('\0', ' '); 
-        System.out.println(indent + "   ├─ Ville : " + nom + " (" + wilaya + ")");
-    }
-}
